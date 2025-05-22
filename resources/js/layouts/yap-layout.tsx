d194707647ac62db41ed9b50a42f62804b87b665
granithalimi <halimigranit6@gmail.com>
2025-05-22 19:07:59 +0200

    layout, fonts

--- resources/js/layouts/yap-layout.tsx
+++ resources/js/layouts/yap-layout.tsx
@@ -1,35 +1,46 @@
 import { useMobileNavigation } from '@/hooks/use-mobile-navigation';
-import { Link, router } from '@inertiajs/react';
-import { LayoutGrid, LogOut } from 'lucide-react';
-import default_profile from '../../assets/images/default.avif';
+import { Head, Link, router } from '@inertiajs/react';
+import { CgProfile } from 'react-icons/cg';
+import { LiaUserFriendsSolid } from 'react-icons/lia';
+import { MdOutlineDashboard } from 'react-icons/md';
 import whitelogo from '../../assets/images/yapster_white_logo.png';
+import '../../assets/style/style.css';
 
-function YapLayout({ children }: any) {
+function YapLayout({ children, title }: any) {
     const cleanup = useMobileNavigation();
     const handleLogout = () => {
         cleanup();
         router.flushAll();
     };
+    console.log(title);
     return (
-        <div className="min-h-screen min-w-full bg-gradient-to-br from-[#2EE59D] to-[#11998e]">
+        <div className="roboto_ min-h-screen min-w-full bg-gradient-to-br from-[#2EE59D] to-[#11998e]">
+            <div className="flex w-full justify-center pt-4 pb-2">
+                <div className="montserrat_ w-11/12 text-3xl">{title}</div>
+            </div>
+            <Head title={title} />
             {children}
-            <div className="fixed bottom-0 flex h-1/12 w-full items-center justify-center bg-[#11998e] md:h-screen md:w-1/6 md:flex-col md:justify-around">
+            <div className="md:ps-1/6 fixed bottom-0 flex h-1/12 w-full items-center justify-center bg-[#11998e] md:h-screen md:w-1/6 md:flex-col md:justify-around">
                 <Link href={route('dashboard')} className="flex w-1/4 flex-col items-center">
-                    <LayoutGrid />
+                    <MdOutlineDashboard className="text-3xl" />
                     <p className="text-xs">Dashboard</p>
+                </Link>
+                <Link className="flex w-1/4 flex-col items-center" href={route('yaps.index')}>
+                    <LiaUserFriendsSolid className="text-3xl" />
+                    <p className="m-0 text-center text-xs">Friends</p>
                 </Link>
                 <Link href={route('yaps.index')} className="flex w-1/4 flex-col items-center">
                     <img src={whitelogo} className="w-7" />
                     <p className="text-xs">Yaps</p>
                 </Link>
                 <Link className="flex w-1/4 flex-col items-center" href={route('profile.edit')}>
-                    <img src={default_profile} className="w-7 rounded-full" />
+                    <CgProfile className="text-3xl" />
                     <p className="m-0 text-center text-xs">Profile</p>
                 </Link>
-                <Link className="flex w-1/4 flex-col items-center" method="post" href={route('logout')} as="button" onClick={handleLogout}>
+                {/* <Link className="flex w-1/4 flex-col items-center" method="post" href={route('logout')} as="button" onClick={handleLogout}>
                     <LogOut />
                     <p className="text-xs">Log out</p>
-                </Link>
+                </Link> */}
             </div>
         </div>
     );
--- resources/js/pages/yaps.tsx
+++ resources/js/pages/yaps.tsx
@@ -1,6 +1,7 @@
 import { useMobileNavigation } from '@/hooks/use-mobile-navigation';
 import YapLayout from '@/layouts/yap-layout';
 import { router } from '@inertiajs/react';
+import { FaSearch } from 'react-icons/fa';
 
 function yaps({ auth }: any) {
     const cleanup = useMobileNavigation();
@@ -9,21 +10,26 @@ function yaps({ auth }: any) {
         router.flushAll();
     };
     return (
-        <YapLayout>
-            <div>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="text-8xl">hello</h1>
-                <h1 className="pb-20 text-8xl">hello</h1>
+        <YapLayout title="Yaps">
+            <div className="flex w-full justify-center pb-4">
+                <div className="relative w-11/12">
+                    <input className="w-full rounded-xl bg-white/20 py-1 ps-7 text-black backdrop-blur-2xl" placeholder={`Search Yaps...`} />
+                    <FaSearch className="absolute top-1/2 left-2 -translate-y-1/2 text-[#11998e]" />
+                </div>
+            </div>
+            <div className="flex w-full justify-center">
+                <div className="w-11/12 pb-20">
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                    <div className="my-3 h-20 w-full rounded-xl bg-white/20 backdrop-blur-3xl"></div>
+                </div>
             </div>
         </YapLayout>
     );
