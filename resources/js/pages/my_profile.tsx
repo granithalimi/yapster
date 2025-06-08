a653f1925be25b028786d4b7f74c246dc11da825
Granit Halimi <granithalimi@Granits-MacBook-Air.local>
2025-06-08 05:29:33 +0200

    adding webcam pics

--- resources/js/pages/my_profile.tsx
+++ resources/js/pages/my_profile.tsx
@@ -3,7 +3,9 @@ import YapLayout from '@/layouts/yap-layout';
 import { Link, router, useForm } from '@inertiajs/react';
 import { useEcho } from '@laravel/echo-react';
 import { LogOut } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { IoMdClose } from "react-icons/io";
+import Webcam from 'react-webcam'
 
 function my_profile({ auth, following, followers, notifs }: any) {
     useEcho(`notif-channel.${auth.user.id}`, 'NotifsEvent', (e: any) => {
@@ -18,8 +20,15 @@ function my_profile({ auth, following, followers, notifs }: any) {
     const [followingDiv, setFollowingDiv] = useState(false);
     const [followings, setFollowing] = useState<any>({});
     const [follower, setFollowers] = useState<any>({});
+    const [camera, setCamera] = useState<any>(false);
+    const [imgSrc, setImgSrc] = useState<any>(null);
+    const webRef = useRef(null);
 
     const { data, setData, delete: destroy } = useForm<any>();
+
+    const capture = (e : any) => {
+        setImgSrc(e.getScreenshot())
+    }
 
     const [haveNotifs, setHaveNotifs] = useState<boolean>(false);
     useEffect(() => {
@@ -69,7 +78,7 @@ function my_profile({ auth, following, followers, notifs }: any) {
                                 <div key={ind} className="mx-auto mb-3 flex h-16 w-11/12 items-center justify-between rounded-lg bg-white/10 text-sm">
                                     <h1 className="ms-2 font-bold">{f.notifs.name}</h1>
                                     <button onClick={(e) => handleRemove(e, f.id)} className="me-2 rounded-md bg-gray-500 px-3 py-1 font-bold">
-                                        remove
+                                        Remove
                                     </button>
                                 </div>
                             ))}
@@ -81,17 +90,19 @@ function my_profile({ auth, following, followers, notifs }: any) {
                                 <div key={ind} className="mx-auto mb-3 flex h-16 w-11/12 items-center justify-between rounded-lg bg-white/10 text-sm">
                                     <h1 className="ms-2 font-bold">{f.users.name}</h1>
                                     <button onClick={(e) => handleRemove(e, f.id)} className="me-2 rounded-md bg-gray-500 px-3 py-1 font-bold">
-                                        remove
+                                        Remove
                                     </button>
                                 </div>
                             ))}
                     </div>
                     <div className="flex h-full w-2/6 flex-col items-center justify-center gap-2">
-                        <img
-                            src={`${window.location.origin}/storage/images/${auth.user.profile}`}
-                            alt="profile_pic"
-                            className="h-16 w-16 rounded-full object-cover"
-                        />
+                        <button onClick={e => setCamera((p:any) => !p)}>
+                            <img
+                                src={`${window.location.origin}/storage/images/${auth.user.profile}`}
+                                alt="profile_pic"
+                                className="h-16 w-16 rounded-full object-cover"
+                            />
+                        </button>
                         <h1 className="text-sm font-bold">{auth.user.name}</h1>
                     </div>
                     <div className="h-full w-4/6">
@@ -121,6 +132,23 @@ function my_profile({ auth, following, followers, notifs }: any) {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className={`${(camera) ? "fixed" : "hidden"} flex justify-center items-center bg-black/40 w-screen h-screen top-0`}>
+                {camera && 
+                <div className={`w-10/12 rounded-xl bg-green-400 flex pt-4 gap-5 flex-col items-end justify-center`}>
+                    <IoMdClose onClick={e => setCamera((p:any) => !p)} className='text-3xl pe-3'/>
+                <Webcam className={`${(imgSrc) ? "hidden" : "block"} w-full`} screenshotFormat='image/jpeg' audio={false} ref={webRef} />
+                    {imgSrc && 
+                        <img src={imgSrc} />
+                    }
+                <div className='w-full flex justify-around pb-4'>
+                    <button className={`${(imgSrc) ? "hidden" : "block"} text-center`} onClick={e => capture(webRef.current)}>take the shit</button>
+                    <button className={`${(imgSrc) ? "block" : "hidden"} text-center`}>post</button>
+                    <button className={`${(imgSrc) ? "block" : "hidden"} text-center`} onClick={e => setImgSrc((p:any) => false)}>take another</button>
+                </div>
+                </div>
+                }
             </div>
         </YapLayout>
     );
